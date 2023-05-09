b0c314c46d678d8d3319844b623d634a42f36cd3
Jonathan Rojas Granda <jonargp1@gmail.com>
2023-05-10 01:29:06 +0200

    Fix profile

--- src/screens/Profile.js
+++ src/screens/Profile.js
@@ -1,5 +1,5 @@
 import React, { useEffect } from 'react'
-import { View, Button, Image, Text } from 'react-native'
+import { View, Button, Image, Text, ActivityIndicator } from 'react-native'
 import { useDispatch } from 'react-redux'
 import AsyncStorage from '@react-native-async-storage/async-storage'
 
@@ -9,7 +9,7 @@ import { signOut as signOutFirebase } from 'firebase/auth'
 
 const Profile = () => {
   const [user, setUser] = React.useState(null)
-  // const [isLoading, setIsLoading] = React.useState(true)
+  const [isLoading, setIsLoading] = React.useState(true)
   const dispatch = useDispatch()
 
   const handleLogout = async () => {
@@ -43,29 +43,42 @@ const Profile = () => {
             .then(data => {
               console.log('DATA', data)
               setUser(data)
+              setIsLoading(false)
             })
             .catch(error => console.log('ERROR', error))
+        } else {
+          setIsLoading(false)
         }
       } catch (error) {
         console.log('getUser', error)
+        setIsLoading(false)
       }
     }
     getUser()
+    console.log('user', user)
   }, [])
+
+  if (isLoading) {
+    return (
+      <View className='flex-1 justify-center align-middle'>
+        <ActivityIndicator size='large' />
+      </View>
+    )
+  }
   return (
     <View className='flex-1 items-center'>
       <View className='flex-row bg-slate-300'>
         <Image source={require('../../assets/adaptive-icon.png')} className='w-32 h-32' />
-        <Text>{user.name}</Text>
+        <Text>{user?.name}</Text>
       </View>
       <View className='bg-slate-400 flex-row'>
         <View>
           <Text>Correo electronico</Text>
-          <Text>{user.email}</Text>
+          <Text>{user?.email}</Text>
         </View>
         <View>
           <Text>Universidad</Text>
-          <Text>{user.university}</Text>
+          <Text>{user?.university}</Text>
         </View>
       </View>
       <Button
